459df219bec5f2fa897b041571607a48c4830db6
Steven Penny <svnpenn+muv1@gmail.com>
2020-04-25 00:03:33 -0500

    +   o_a.pathname = 'listen';

--- docs/js/github.js
+++ docs/js/github.js
@@ -3,7 +3,7 @@
 export const f_github = function (song) {
    // <a>
    const o_a = new URL(location.origin);
-   o_a.pathname = 'umber/listen';
+   o_a.pathname = 'listen';
    o_a.searchParams.set('v', song.post);
    // <img>
    const o_img = new URL('https://github.com');
--- docs/js/listen.js
+++ docs/js/listen.js
@@ -1,18 +1,35 @@
 'use strict';
-const gei = s1 => document.getElementById(s1);
-const path = 'https://github.com/muv1/umber/releases/download';
+
+function f_json(o_resp) {
+   return o_resp.json();
+}
+
+function f_listen(a_juke) {
+   const a_track = a_juke.find(f_track);
+   const n_song = a_track[0];
+   const n_year = a_track[1];
+   const s_album = a_track[2].slice(2);
+   const s_title = a_track[3];
+   const s_date = new Date(n_song * 1000).toDateString();
+   document.title = s_title + ' - Umber Listen';
+   document.getElementById('artist').textContent = s_title;
+
+   const o_date = document.getElementById('date');
+   o_date.textContent = 'released ' + n_year + ' - posted ' + s_date;
+
+   const o_listen = document.getElementById('listen');
+   o_listen.src = s_path + '/' + s_album + '/' + n_song;
+   o_listen.poster = s_path + '/' + s_album + '/image.jpg';
+}
+
+function f_track(a_track) {
+   return a_track[0] == n_v;
+}
+
+const s_v = new URLSearchParams(location.search).get('v');
+const n_v = +(s_v);
+const s_path = 'https://github.com/muv1/umber/releases/download';
 const s_json = '../umber.json';
-
-fetch(s_json).
-then(resp => resp.json()).
-then(a1 => {
-   const n_id = new URLSearchParams(location.search).get('v');
-   const vdeo = a1.find(trck => trck[0] == n_id);
-   const dstr = new Date(vdeo[0] * 1000).toDateString();
-
-   gei('listen').src = path + '/' + vdeo[2].slice(2) + '/' + vdeo[0];
-   gei('listen').poster = path + '/' + vdeo[2].slice(2) + '/image.jpg';
-   gei('artist').textContent = vdeo[3];
-   gei('date').textContent = 'released ' + vdeo[1] + ' - posted ' + dstr;
-   document.title = vdeo[3] + ' - Umber Listen';
-});
+const o_fetch = fetch(s_json);
+const o_json = o_fetch.then(f_json);
+o_json.then(f_listen);
