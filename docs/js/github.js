e12cb80f1cb280a0726927d14096936417ccc3ab
Steven Penny <svnpenn+muv1@gmail.com>
2020-05-18 15:30:31 -0500

    -         "$list": "white",

--- docs/js/github.js
+++ docs/js/github.js
@@ -6,8 +6,8 @@ export function f_github(s_id_1, s_id_2) {
    o_a.href = location.origin + '/umber/listen?v=' + s_id_1;
    // <img>
    const o_img = document.createElement('img');
-   const s_path = '/muv1/umber/releases/download/' + s_id_2 + '/image.jpg';
-   o_img.src = 'https://github.com' + s_path;
+   o_img.src = 'https://github.com/muv1/umber/releases/download/' +
+   s_id_2 + '/' + s_id_2 + '.jpg';
    // return
    o_a.append(o_img);
    return o_a;
--- docs/js/listen.js
+++ docs/js/listen.js
@@ -7,18 +7,20 @@ function f1_json(o_resp) {
 
 function f2_listen(a_table) {
    const a_row = a_table.find(f3_track);
-   const s_songid = a_row[0];
+   const s_song = a_row[0];
    const s_year = a_row[1].toString();
-   const s_albumid = a_row[2].slice(2);
+   const a_album = a_row[2].split('/');
+   const s_album = a_album[1];
+   const s_ext = a_album[2];
    const s_title = a_row[3];
    document.title = s_title + ' - Umber Listen';
    document.getElementById('artist').textContent = s_title;
    const o_date = document.getElementById('date');
-   o_date.textContent = 'released ' + s_year + ' - posted ' + f_date(s_songid);
+   o_date.textContent = 'released ' + s_year + ' - posted ' + f_date(s_song);
    const o_listen = document.getElementById('listen');
    const s_path = 'https://github.com/muv1/umber/releases/download';
-   o_listen.src = s_path + '/' + s_albumid + '/' + s_songid;
-   o_listen.poster = s_path + '/' + s_albumid + '/image.jpg';
+   o_listen.src = s_path + '/' + s_album + '/' + s_song + '.' + s_ext;
+   o_listen.poster = s_path + '/' + s_album + '/' + s_album + '.jpg';
 }
 
 function f3_track(a_row) {
