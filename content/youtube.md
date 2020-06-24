876b0cd1812805b6ed2c19882dc98d5b9447ff80
Steven Penny <svnpenn+nu8@gmail.com>
2020-06-24 14:17:28 -0500

    new file:   content/youtube.md

---
+++ content/youtube.md
@@ -0,0 +1,9 @@
+# YouTube
+
+`img` over `iframe`:
+
+- `iframe` requires embedly in some cases
+- Killing embeds makes loading faster
+- Links will allow use of CSS `:focus` or `:active`
+- Not all links can be embedded
+- Using embeds breaks keyboard shortcuts
--- docs/js/youtube.js
+++ docs/js/youtube.js
@@ -3,6 +3,6 @@
 export function f_youtube(s_id_2) {
    const m = {};
    m.href = 'https://www.youtube.com/watch?v=' + s_id_2;
-   m.src = 'https://i.ytimg.com/vi/' + s_id_2 + '/sd1.jpg';
+   m.src = 'https://i.ytimg.com/vi/' + s_id_2 + '/sddefault.jpg';
    return m;
 }
