d99bdf9605b1caf1f4e2e976aaa830f9c6e415c9
Steven Penny <svnpenn+nu8@gmail.com>
2020-08-24 00:26:46 -0500

    +function Radix64() {

--- js/date.js
+++ js/date.js
@@ -1,19 +1,24 @@
 'use strict';
-const s_dig = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
 
-function r64_decode(s_in) {
-   let n_out = 0;
-   for (const s_chr of s_in) {
-      n_out = n_out * 64 + s_dig.indexOf(s_chr);
-   }
-   return n_out;
+function Radix64() {
+   let s = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+   this.s_dig = s;
 }
 
+Radix64.prototype.decode = function(s_in) {
+   let n_out = 0;
+   for (let s_chr of s_in) {
+      n_out = n_out * 64 + this.s_dig.indexOf(s_chr);
+   }
+   return n_out;
+};
+
 export function f_date(s_id) {
-   const n_id = r64_decode(s_id);
-   const o = new Date(n_id * 1000);
-   const s_day = o.toLocaleString(0, {weekday: 'short'});
-   const s_mon = o.toLocaleString(0, {month: 'short', day: 'numeric'});
-   const s_year = o.toLocaleString(0, {year: 'numeric'});
+   const o_rad = new Radix64;
+   const n_id = o_rad.decode(s_id);
+   const o_dat = new Date(n_id * 1000);
+   const s_day = o_dat.toLocaleString(0, {weekday: 'short'});
+   const s_mon = o_dat.toLocaleString(0, {month: 'short', day: 'numeric'});
+   const s_year = o_dat.toLocaleString(0, {year: 'numeric'});
    return s_day + ' ' + s_mon + ' ' + s_year;
 }
