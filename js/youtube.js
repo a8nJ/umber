504cffdce5df074360aa8406dbd71f215758a422
Steven Penny <svnpenn+nu8@gmail.com>
2020-09-10 07:49:46 -0500

    +   const m = {href: 'https://www.youtube.com/watch?v=' + s_id_2};

--- js/youtube.js
+++ js/youtube.js
@@ -1,8 +1,7 @@
 'use strict';
 
 export function f_youtube(s_id_2, s_id_3) {
-   const m = {};
-   m.href = 'https://www.youtube.com/watch?v=' + s_id_2;
+   const m = {href: 'https://www.youtube.com/watch?v=' + s_id_2};
    if (s_id_3 != '') {
       m.src = 'https://i.ytimg.com/vi/' + s_id_2 + '/' + s_id_3 + '.jpg';
    } else {
