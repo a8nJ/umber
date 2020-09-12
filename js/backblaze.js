91bb37759ea13aca2608f096e9fa101acdb397f7
Steven Penny <svnpenn+nu8@gmail.com>
2020-09-12 11:39:13 -0500

    +   return {

--- js/backblaze.js
+++ js/backblaze.js
@@ -1,8 +1,8 @@
 'use strict';
 
 export function f_backblaze(s_id_1, s_id_2) {
-   const m = {};
-   m.href = location.origin + '/umber/backblaze?v=' + s_id_1;
-   m.src = 'https://f002.backblazeb2.com/file/0Tl4aD/' + s_id_2 + '.jpg';
-   return m;
+   return {
+      href: location.origin + '/umber/backblaze?v=' + s_id_1,
+      src: 'https://f002.backblazeb2.com/file/0Tl4aD/' + s_id_2 + '.jpg'
+   };
 }
