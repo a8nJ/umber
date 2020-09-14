4d561ed3f4b929ab35298befde582302d0d4a47f
Steven Penny <svnpenn+nu8@gmail.com>
2020-09-14 07:21:14 -0500

    +   const m_in = {

--- js/bandcamp.js
+++ js/bandcamp.js
@@ -1,19 +1,21 @@
 'use strict';
 
 export function f_bandcamp(s_id_2, s_id_3) {
-   const m_in = {};
-   // optional
-   m_in.artwork = 'small';
-   // required when protocol is not "file:"
-   m_in.ref = '';
-   // optional
-   m_in.size = 'large';
-   // required
-   m_in.track = s_id_2;
-   const m_out = {};
-   // case sensitive
+   const m_in = {
+      // optional
+      artwork: 'small',
+      // required when protocol is not "file:"
+      ref: '',
+      // optional
+      size: 'large',
+      // required
+      track: s_id_2
+   };
    const o = new URLSearchParams(m_in);
-   m_out.href = 'https://bandcamp.com/EmbeddedPlayer?' + o.toString();
-   m_out.src = 'https://f4.bcbits.com/img/' + s_id_3 + '_5.jpg';
-   return m_out;
+   const s = o.toString();
+   return {
+      // case sensitive
+      href: 'https://bandcamp.com/EmbeddedPlayer?' + s,
+      src: 'https://f4.bcbits.com/img/' + s_id_3 + '_5.jpg'
+   };
 }
