b7e10ad1014cffac0c5a538744f138fd82b508d6
Steven Penny <svnpenn+nu8@gmail.com>
2020-10-20 00:04:23 -0500

    +const Radix64 = {

--- js/date.js
+++ js/date.js
@@ -1,21 +1,18 @@
 'use strict';
 
-function Radix64() {
-   let s = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
-   this.s_dig = s;
-}
-
-Radix64.prototype.decode = function (s_in) {
-   let n_out = 0;
-   for (let s_chr of s_in) {
-      n_out = n_out * 64 + this.s_dig.indexOf(s_chr);
+const Radix64 = {
+   digits: '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz',
+   decode(s_in) {
+      let n_out = 0;
+      for (let s_chr of s_in) {
+         n_out = n_out * 64 + this.digits.indexOf(s_chr);
+      }
+      return n_out;
    }
-   return n_out;
 };
 
 export function f_date(s_id) {
-   const o_rad = new Radix64;
-   const n_id = o_rad.decode(s_id);
+   const n_id = Radix64.decode(s_id);
    const o_date = new Date(n_id * 1000);
    const o_fmt = new Intl.DateTimeFormat('en', {
       day: 'numeric', month: 'short', weekday: 'short', year: 'numeric'
