defc37b3e79cbcd2a6d880da253539373989211c
Steven Penny <0ua8nj@gmail.com>
2020-11-10 15:54:55 -0600

    +function JsonF(RespO) {

--- js/umber.js
+++ js/umber.js
@@ -5,116 +5,116 @@ import {DateF} from '/umber/js/date.js';
 import {SoundcloudF} from '/umber/js/soundcloud.js';
 import {YoutubeF} from '/umber/js/youtube.js';
 
-function JsonF(o_resp) {
-   return o_resp.json();
+function JsonF(RespO) {
+   return RespO.json();
 }
 
-function DataF(a_table) {
-   const o_par = new URLSearchParams(location.search);
+function DataF(TableA) {
+   const ParO = new URLSearchParams(location.search);
    // 1. filter
-   if (o_par.has('q')) {
-      const s_query = o_par.get('q');
-      function FilterF(a_row) {
+   if (ParO.has('q')) {
+      const QueryS = ParO.get('q');
+      function FilterF(RowA) {
          /* for now, we are going to match on just the artist and recording. if
          we later decide to match other items, its tempting to just join the
          array and match on that. however the year is a number, and some
          languages dont allow arrays of different types. so if the time comes,
          handle each element individually rather than trying to join the
          array. */
-         const s_song = a_row[3];
-         return RegExp(s_query, 'i').test(s_song);
+         const SongS = RowA[3];
+         return RegExp(QueryS, 'i').test(SongS);
       }
-      a_table = a_table.filter(FilterF);
+      TableA = TableA.filter(FilterF);
    }
    // 2. slice
-   let n_begin = 0;
-   if (o_par.has('v')) {
-      const s_v = o_par.get('v');
-      function IndexF(a_row) {
+   let BeginN = 0;
+   if (ParO.has('v')) {
+      const IdS = ParO.get('v');
+      function IndexF(RowA) {
          // account for deleted entries
-         return a_row[0] <= s_v;
+         return RowA[0] <= IdS;
       }
-      n_begin = a_table.findIndex(IndexF);
-      if (n_begin == -1) {
-         n_begin = 0;
+      BeginN = TableA.findIndex(IndexF);
+      if (BeginN == -1) {
+         BeginN = 0;
       }
-      document.title = 'Umber - ' + DateF(s_v);
+      document.title = 'Umber - ' + DateF(IdS);
    }
-   const n_page = 30;
-   const a_slice = a_table.slice(n_begin, n_begin + n_page);
-   const o_figs = document.getElementById('figures');
-   for (const a_row of a_slice) {
-      const o_fig = f3_figure(a_row);
-      o_figs.append(o_fig);
+   const PageN = 30;
+   const SliceA = TableA.slice(BeginN, BeginN + PageN);
+   const FigsO = document.getElementById('figures');
+   for (const RowA of SliceA) {
+      const FigO = FigureF(RowA);
+      FigsO.append(FigO);
    }
-   const o_old = document.getElementById('older');
-   const n_old = n_begin + n_page;
-   if (n_old < a_table.length) {
-      const s_v = a_table[n_old][0];
-      o_par.set('v', s_v);
-      o_old.href = '?' + o_par.toString();
+   const OldO = document.getElementById('older');
+   const OldN = BeginN + PageN;
+   if (OldN < TableA.length) {
+      const IdS = TableA[OldN][0];
+      ParO.set('v', IdS);
+      OldO.href = '?' + ParO.toString();
    } else {
-      o_old.remove();
+      OldO.remove();
    }
-   const o_new = document.getElementById('newer');
-   const n_new = n_begin - n_page;
-   if (n_new >= 0) {
-      const s_v = a_table[n_new][0];
-      o_par.set('v', s_v);
-      o_new.href = '?' + o_par.toString();
+   const NewO = document.getElementById('newer');
+   const NewN = BeginN - PageN;
+   if (NewN >= 0) {
+      const IdS = TableA[NewN][0];
+      ParO.set('v', IdS);
+      NewO.href = '?' + ParO.toString();
    } else {
-      o_new.remove();
+      NewO.remove();
    }
 }
 
-function f3_figure(a_row) {
+function FigureF(RowA) {
    // column 0
-   const s_id_1 = a_row[0];
+   const DateS = RowA[0];
    // column 1
-   const s_year = a_row[1].toString();
+   const YearS = RowA[1].toString();
    // column 2
-   const a_host = a_row[2].split('/');
-   const s_site = a_host[0];
-   const s_id_2 = a_host[1];
-   let s_id_3 = '';
-   if (a_host.length == 3) {
-      s_id_3 = a_host[2];
+   const HostA = RowA[2].split('/');
+   const SiteS = HostA[0];
+   const AudioS = HostA[1];
+   let VideoS = '';
+   if (HostA.length == 3) {
+      VideoS = HostA[2];
    }
    // column 3
-   const s_title = a_row[3];
+   const TitleS = RowA[3];
    let m;
-   switch (s_site) {
+   switch (SiteS) {
    case 'b':
-      m = BandcampF(s_id_2, s_id_3);
+      m = BandcampF(AudioS, VideoS);
       break;
    case 'm4a':
    case 'mp3':
    case 'mp4':
-      m = BackblazeF(s_id_1, s_id_2);
+      m = BackblazeF(DateS, AudioS);
       break;
    case 's':
-      m = SoundcloudF(s_id_2, s_id_3);
+      m = SoundcloudF(AudioS, VideoS);
       break;
    case 'y':
-      m = YoutubeF(s_id_2, s_id_3);
+      m = YoutubeF(AudioS, VideoS);
    }
    // part 1
-   const o_temp = document.querySelector('#temp');
+   const TempO = document.querySelector('#temp');
    // part 2
-   const o_fig = o_temp.content.cloneNode(true);
+   const FigO = TempO.content.cloneNode(true);
    // part 3
-   const o_cap_a = o_fig.querySelector('figcaption a');
-   const o_img = o_fig.querySelector('img');
-   const o_img_a = o_fig.querySelector('a');
-   const o_time = o_fig.querySelector('time');
-   o_cap_a.href = m.href;
-   o_cap_a.target = '_blank';
-   o_cap_a.textContent = s_title;
-   o_img.src = m.src;
-   o_img_a.href = m.href;
-   o_img_a.target = '_blank';
-   o_time.textContent = 'released ' + s_year + ' - posted ' + DateF(s_id_1);
-   return o_fig;
+   const CapAnO = FigO.querySelector('figcaption a');
+   const ImgO = FigO.querySelector('img');
+   const ImgAnO = FigO.querySelector('a');
+   const TimeO = FigO.querySelector('time');
+   CapAnO.href = m.href;
+   CapAnO.target = '_blank';
+   CapAnO.textContent = TitleS;
+   ImgO.src = m.src;
+   ImgAnO.href = m.href;
+   ImgAnO.target = '_blank';
+   TimeO.textContent = 'released ' + YearS + ' - posted ' + DateF(DateS);
+   return FigO;
 }
 
 fetch('/umber/umber.json').then(JsonF).then(DataF);
