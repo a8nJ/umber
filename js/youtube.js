956beaf2221aeff226ec5b6bcbd38b211d1002ec
Steven Penny <srpen6@gmail.com>
2021-06-26 15:30:00 -0500

    +      m.src += 'sddefault.webp';

--- js/youtube.js
+++ js/youtube.js
@@ -3,11 +3,17 @@
 export function youtube(param) {
    const m = {};
    m.href = 'https://www.youtube.com/watch?v=' + param.get('b');
-   m.src = 'https://i.ytimg.com/vi/' + param.get('b') + '/';
    if (param.has('c')) {
-      m.src += param.get('c') + '.jpg';
+      m.src = param.get('c');
    } else {
-      m.src += 'sddefault.jpg';
+      m.src = 'sddefault.webp';
+   }
+   m.src = param.get('b') + '/' + m.src;
+   // need HTTPS to avoid "Parts of this page are not secure"
+   if (m.src.endsWith('.webp')) {
+      m.src = 'https://i.ytimg.com/vi_webp/' + m.src;
+   } else {
+      m.src = 'https://i.ytimg.com/vi/' + m.src;
    }
    return m;
 }
