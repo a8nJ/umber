d70cc51740b581a4205094e5baa3e1ae51a9a2db
Steven Penny <srpen6@gmail.com>
2021-07-14 09:28:45 -0500

    +const fmts = [

--- js/date.js
+++ js/date.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const fmt = [
+const fmts = [
    {weekday: 'short'}, {month: 'short'}, {day: 'numeric'}, {year: 'numeric'}
 ];
 
 export function getDate(id) {
    const parse = parseInt(id, 36);
    const date = new Date(parse * 1000);
-   return join(date, fmt, ' ');
+   return join(date, fmts, ' ');
 }
 
-function join(t, a, s) {
-   function format(m) {
-      let f = new Intl.DateTimeFormat('en', m);
-      return f.format(t);
+function join(date, fmts, sep) {
+   function format(fmt) {
+      let time = new Intl.DateTimeFormat('en', fmt);
+      return time.format(date);
    }
-   return a.map(format).join(s);
+   return fmts.map(format).join(sep);
 }
